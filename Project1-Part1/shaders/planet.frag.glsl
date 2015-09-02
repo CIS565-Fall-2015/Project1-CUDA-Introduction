91e3e063eedbb17a03805ba1e3ca28347ff51a04
shrekshao <shrekshao@gmail.com>
2015-09-01 23:01:53 -0400

    part-1-n-body

--- Project1-Part1/shaders/planet.frag.glsl
+++ Project1-Part1/shaders/planet.frag.glsl
@@ -2,6 +2,10 @@
 
 out vec4 fragColor;
 
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
 void main() {
-    fragColor = vec4(1.0);
+    fragColor = vec4( rand(gl_FragCoord.yz) ,rand(gl_FragCoord.zx),rand(gl_FragCoord.xy),1.0);
 }
